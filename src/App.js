440752dfc29d70c1f5a88fbef12553f802332095
SMAkhup <krzysztof.huptys@sma-magnetics.com>
2021-04-24 18:57:53 +0200

    working mouse follower

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { Face } from './components/Face';
 import { range } from 'd3';
 
 const width = 166;
 const height = 166;
-const array = range(3*6);
+const initialMousePosition = { x: width / 2, y: height / 2};
 
-export const App = () => array.map(() => (
-    <Face
-        width={width}
-        height={height}
-        centerX={width / 2}
-        centerY={height / 2}
-        strokeWidth={10}
-        eyeOffsetX={30}
-        eyeOffsetY={30}
-        eyeRadius={5 + Math.random() * 10}
-        mouthRadius={7 + Math.random() * 9}
-        mouthWidth={2 + Math.random() * 10}
-    />
-));
+export const App = () => {
+    const [mousePosition, setMousePosition] = useState(initialMousePosition);
+ 
+    const handleMouseMove = useCallback(event => {
+        const { clientX, clientY } = event;
+
+        setMousePosition({ x: clientX, y: clientY });
+    }, [setMousePosition]);
+
+    return (
+        <div className="container" onMouseMove={handleMouseMove}>
+            <Face
+                width={width}
+                height={height}
+                centerX={mousePosition.x}
+                centerY={mousePosition.y}
+                strokeWidth={10}
+                eyeOffsetX={30}
+                eyeOffsetY={30}
+                eyeRadius={5 + Math.random() * 10}
+                mouthRadius={7 + Math.random() * 50}
+                mouthWidth={2 + Math.random() * 10}
+            />
+        </div>
+    )
+};
